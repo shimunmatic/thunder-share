49b0901bc8e548646c881a142ca216b61055ccb0
Shimun Matic <shimun.matic@gmail.com>
2019-07-13 14:55:47 +0200

    Initial commit

---
+++ src/main/java/com/shimunmatic/springshowcase/SpringShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.shimunmatic.springshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShowcaseApplication.class, args);
+    }
+
+}
