21e9a1f84a9f753206b1c9b04cf0bf8df7a42186
Shimun Matic <shimun.matic@gmail.com>
2019-09-05 21:03:47 +0200

    Removed context loads test

--- server/src/test/java/com/shimunmatic/thundershare/ThunderShareApplicationTests.java
+++ server/src/test/java/com/shimunmatic/thundershare/ThunderShareApplicationTests.java
@@ -2,14 +2,10 @@ package com.shimunmatic.thundershare;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@EnableJpaRepositories
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@EnableJpaRepositories
 public class ThunderShareApplicationTests {
 
     //  @Test
