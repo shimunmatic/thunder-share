e33019616b7785500ca6eeae0916ba7064f442e2
shimunmatic <shimun.matic@gmail.com>
2019-12-07 14:24:41 +0100

    Simple tests

---
+++ server/src/test/java/com/shimunmatic/thundershare/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.shimunmatic.thundershare;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void testExample() {
+        Assert.assertEquals(1, Integer.parseInt("1"));
+    }
+}
--- server/src/test/java/com/shimunmatic/thundershare/ThunderShareApplicationTests.java
+++ server/src/test/java/com/shimunmatic/thundershare/ThunderShareApplicationTests.java
@@ -1,20 +1,17 @@
 package com.shimunmatic.thundershare;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@EnableJpaRepositories
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableJpaRepositories
 public class ThunderShareApplicationTests {
 
-    //  @Test
+    @Test
     public void contextLoads() {
     }
-
-    @Test
-    public void testExample() {
-        Assert.assertEquals(1, Integer.parseInt("1"));
-    }
-
 }
